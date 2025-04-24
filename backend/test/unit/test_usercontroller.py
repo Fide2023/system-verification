833951808299786a0b27791f850906cc72ded4ed
Filip <fide23@student.bth.se>
2025-04-24 05:25:04 +0200

    utalized parametrize to remove two test functions

--- backend/test/unit/test_usercontroller.py
+++ backend/test/unit/test_usercontroller.py
@@ -5,22 +5,28 @@ from  src.controllers.usercontroller import UserController
 
 
 class TestGetUserByEmail:
-    def test_valid_email(self):
-        # Testing a valid email, should return a user object
-        email = "jane.doe@gmail.com"
-        expected_user = [{"email": "jane.doe@gmail.com", "firstName": "Jane", "lastName": "Doe"}]
+    #1. valid email, one user
+    #2. valid email, multiple users
+    #3. valid email, no user
+    @pytest.mark.parametrize('email, users, expected', [
+        ("jane.doe@gmail.com", [{"email": "jane.doe@gmail.com", "firstName": "Jane", "lastName": "Doe"}], {"email": "jane.doe@gmail.com", "firstName": "Jane", "lastName": "Doe"}),
+        ("jane.doe@gmail.com", [{"email": "jane.doe@gmail.com", "firstName": "Jane", "lastName": "Doe"}, {"email": "jane.doe@gmail.com", "firstName": "Shane", "lastName": "Shoe"}], {"email": "jane.doe@gmail.com", "firstName": "Jane", "lastName": "Doe"}),
+        ("jane.doe@gmail.com", None, None),
+
+    ])
+    def test_valid_email(self, email, users, expected):
 
         # Mock using MagicMock
         mockedDAO = mock.MagicMock()
-        # Set return value
-        mockedDAO.find.return_value = expected_user
+        # Set return value to users
+        mockedDAO.find.return_value = users
 
         # Call function
         controller = UserController(dao=mockedDAO)
         result = controller.get_user_by_email(email)
 
-        # Assertions
-        assert result == expected_user[0]
+        # Assert result is equal to expected
+        assert result == expected
         mockedDAO.find.assert_called_once_with({"email": email})
 
     def test_invalid_email(self):
@@ -32,30 +38,6 @@ class TestGetUserByEmail:
 
         with pytest.raises(ValueError, match="Error: invalid email address"):
             controller.get_user_by_email(email)
-
-    def test_valid_email_multiple_users(self, capsys):
-        # Testing a valid email with multiple users, should return the first user and print an message
-        email = "jane.doe@gmail.com"
-        expected_users = [
-            {"email": "jane.doe@gmail.com", "firstName": "Jane", "lastName": "Doe"}, 
-            {"email": "jane.doe@gmail.com", "firstName": "Shane", "lastName": "Shoe"}
-        ]
-
-
-        mockedDAO = mock.MagicMock()
-        # Set return value
-        mockedDAO.find.return_value = expected_users
-
-        # Call function
-        controller = UserController(dao=mockedDAO)
-        result = controller.get_user_by_email(email)
-        captured = capsys.readouterr()
-
-        # Assertions
-        assert result == expected_users[0]
-        assert f'Error: more than one user found with mail {email}' in captured.out
-        mockedDAO.find.assert_called_once_with({"email": email})
-
 
     def test_database_operation_failure(self):
         # Test database failure, should return an Exception error
@@ -69,26 +51,3 @@ class TestGetUserByEmail:
         controller = UserController(dao=mockedDAO)
         with pytest.raises(Exception, match="Database error"):
             controller.get_user_by_email(email)
-
-
-    def test_email_with_no_user(self):
-        # Test an email with no user, should return None
-        email = "jane.doe@gmail.com"
-        expected_user = None
-
-        # Mock using MagicMock
-        mockedDAO = mock.MagicMock()
-        # Set return value
-        mockedDAO.find.return_value = expected_user
-
-        # Call function
-        controller = UserController(dao=mockedDAO)
-        result = controller.get_user_by_email(email)
-
-        # Assertions
-        assert result == expected_user
-        mockedDAO.find.assert_called_once_with({"email": email})
-
-
-
-
